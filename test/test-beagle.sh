751ec8e206d6e58f5f85c9287898236651491294
Alex Smith <alex@alex-smith.me.uk>
2011-06-30 15:44:01 +0100

    Optimisation to test-beagle.sh, don't decompress the 256MB NAND image if it's already been done.

--- test/test-beagle.sh
+++ test/test-beagle.sh
@@ -25,5 +25,7 @@ echo 'bootm 80200000 81600000' >> boot.scr.tmp
 mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Kiwi" -d boot.scr.tmp build/arm-omap3/boot.scr
 rm -f boot.scr.tmp
 
-zcat test/beagle-nand.bin.gz > build/arm-omap3/beagle-nand.bin
+if [ ! -f build/arm-omap3/beagle-nand.bin ]; then
+	zcat test/beagle-nand.bin.gz > build/arm-omap3/beagle-nand.bin
+fi
 qemu-system-arm -M beagle -mtdblock build/arm-omap3/beagle-nand.bin -sd fat:build/arm-omap3 -serial stdio
