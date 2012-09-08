3a28d0b56ae911b9f83c68bd05685e64c8c6e55d
Alex Smith <alex@alex-smith.me.uk>
2012-09-08 11:15:02 +0100

    Split PL011 code off into a generic directory, so it can be reused by other boards in the future.

--- source/platform/bcm2835/include/bcm2835/uart.h
+++ source/platform/bcm2835/include/bcm2835/uart.h
@@ -22,38 +22,6 @@
 #ifndef __BCM2835_UART_H
 #define __BCM2835_UART_H
 
-/** PL011 UART port definitions. */
-#define PL011_REG_DR		0	/**< Data Register. */
-#define PL011_REG_RSR		1	/**< Receive Status Register. */
-#define PL011_REG_ECR		1	/**< Error Clear Register. */
-#define PL011_REG_FR		6	/**< Flag Register. */
-#define PL011_REG_IBRD		9	/**< Integer Baud Rate Register. */
-#define PL011_REG_FBRD		10	/**< Fractional Baud Rate Register. */
-#define PL011_REG_LCRH		11	/**< Line Control Register. */
-#define PL011_REG_CR		12	/**< Control Register. */
-#define PL011_REG_IFLS		13	/**< Interrupt FIFO Level Select Register. */
-#define PL011_REG_IMSC		14	/**< Interrupt Mask Set/Clear Register. */
-#define PL011_REG_RIS		15	/**< Raw Interrupt Status Register. */
-#define PL011_REG_MIS		16	/**< Masked Interrupt Status Register. */
-#define PL011_REG_ICR		17	/**< Interrupt Clear Register. */
-#define PL011_REG_DMACR		18	/**< DMA Control Register. */
-
-/** PL011 flag register bits. */
-#define PL011_FR_TXFF		(1<<5)	/**< Transmit FIFO full. */
-#define PL011_FR_RXFE		(1<<4)	/**< Receive FIFO empty. */
-
-/** PL011 line control register bits. */
-#define PL011_LCRH_FEN		(1<<4)	/**< Enable FIFOs. */
-#define PL011_LCRH_WLEN5	(0<<5)	/**< 5 data bits. */
-#define PL011_LCRH_WLEN6	(1<<5)	/**< 6 data bits. */
-#define PL011_LCRH_WLEN7	(2<<5)	/**< 7 data bits. */
-#define PL011_LCRH_WLEN8	(3<<5)	/**< 8 data bits. */
-
-/** PL011 control register bit definitions. */
-#define PL011_CR_UARTEN		(1<<0)	/**< UART enable. */
-#define PL011_CR_TXE		(1<<8)	/**< Transmit enable. */
-#define PL011_CR_RXE		(1<<9)	/**< Receive enable. */
-
 /** Base clock rate (3MHz, APB clock rate). */
 #define UART0_CLOCK		3000000
 
--- source/platform/bcm2835/uart.c
+++ source/platform/bcm2835/uart.c
@@ -23,77 +23,23 @@
  * I've seen uses the full PL011. Both use the same RXD/TXD GPIO pins, which
  * one the pins are connected to is determined by which alternate function is
  * set for the GPIO pins (mini UART = ALT5, PL011 = ALT0).
- *
- * Reference:
- *  - PrimeCell UART (PL011) Technical Reference Manual
- *    http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0183g/index.html
- *
- * @todo		Add a generic PL011 implementation, there'll be other
- *			boards that use a PL011 so we don't want to duplicate
- *			code for all of them.
  */
 
 #include <bcm2835/bcm2835.h>
 #include <bcm2835/gpio.h>
 #include <bcm2835/uart.h>
 
-#include <console.h>
+#include <pl011/pl011.h>
+
 #include <loader.h>
-
-/** Baud rate to use. */
-#define BAUD_RATE	115200
-
-/** Mapping of the UART registers. */
-static volatile uint32_t *uart_mapping = (volatile uint32_t *)BCM2835_UART0_BASE;
-
-/** Write a character to the UART console.
- * @param ch		Character to write. */
-static void uart_console_putch(char ch) {
-	/* Wait until the transmit FIFO has space. */
-	while(uart_mapping[PL011_REG_FR] & PL011_FR_TXFF);
-
-	uart_mapping[PL011_REG_DR] = ch;
-}
-
-/** UART console. */
-static console_t uart_console = {
-	.putch = uart_console_putch,
-};
 
 /** Initialise the UART console. */
 void uart_init(void) {
-	uint32_t divider, fraction;
-
-	/* Disable the UART while we configure it. */
-	uart_mapping[PL011_REG_CR] = 0;
-
 	/* Configure the TXD and RXD pins to point to the PL011 UART (ALT0). */
 	gpio_select_function(14, GPIO_FUNC_ALT0);
 	gpio_select_function(15, GPIO_FUNC_ALT0);
 
-	/* Calculate the baud rate divisor registers. See PL011 Reference
-	 * Manual, page 3-10.
-	 *  Baud Rate Divisor = UARTCLK / (16 * Baud Rate)
-	 * This is split into an integer and a fractional part.
-	 *  FBRD = Round((64 * (UARTCLK % (16 * Baud Rate))) / (16 * Baud Rate))
-	 */
-	divider = UART0_CLOCK / (16 * BAUD_RATE);
-	fraction = (8 * (UART0_CLOCK % (16 * BAUD_RATE))) / BAUD_RATE;
-	fraction = (fraction >> 1) + (fraction & 1);
-
-	uart_mapping[PL011_REG_IBRD] = divider;
-	uart_mapping[PL011_REG_FBRD] = fraction;
-
-	/* Initialize the line control register (8N1, FIFOs enabled). Note that
-	 * a write to the LCR is required for an IBRD/FBRD change to take
-	 * effect. */
-	uart_mapping[PL011_REG_LCRH] = PL011_LCRH_FEN | PL011_LCRH_WLEN8;
-
-	/* Enable the UART. */
-	uart_mapping[PL011_REG_CR] = PL011_CR_UARTEN | PL011_CR_TXE | PL011_CR_RXE;
-
-	/* Set the debug console. */
-	debug_console = &uart_console;
+	pl011_init(BCM2835_UART0_BASE, UART0_CLOCK);
 
 	dprintf("Hello, World!\n");
 	while(1);
