4d38eda2c5013f98c91bb5a47a3df697a3ec89c1
Alex Smith <alex@alex-smith.me.uk>
2012-09-12 16:31:59 +0100

    Added STATIC_ASSERT macro.

--- source/include/compiler.h
+++ source/include/compiler.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Alex Smith
+ * Copyright (C) 2011-2012 Alex Smith
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -40,5 +40,8 @@
 
 #define STRINGIFY(val)		#val
 #define XSTRINGIFY(val)		STRINGIFY(val)
+
+#define STATIC_ASSERT(cond)	\
+	do { struct __static_assert { char static_assert_failed[(cond) ? 1 : -1]; }; } while(0)
 
 #endif /* __COMPILER_H */
