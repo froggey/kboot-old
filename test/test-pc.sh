d7db418dd3a2eea5f56673031b3980d50972b413
Alex Smith <alex@alex-smith.me.uk>
2012-12-26 22:20:21 +0000

    Combined 32- and 64-bit test scripts.

--- test/test-pc.sh
+++ test/test-pc.sh
@@ -1,17 +1,21 @@
 #!/bin/bash -ex
 
 scons
-scons build/x86-pc/test/test32.elf
+scons test
 
 mkdir -p isobuild/boot
 cat build/x86-pc/source/platform/pc/stage1/cdboot build/x86-pc/source/loader > isobuild/boot/cdboot.img
-cp build/x86-pc/test/test32.elf isobuild/test.elf
+cp build/x86-pc/test/test32.elf build/x86-pc/test/test64.elf isobuild/
 
 cat > isobuild/boot/loader.cfg << EOF
 set "timeout" 5
 
 entry "Test (32-bit)" {
-	kboot "/test.elf" ["/test.elf"]
+	kboot "/test32.elf" ["/test32.elf"]
+}
+
+entry "Test (64-bit)" {
+	kboot "/test64.elf" ["/test64.elf"]
 }
 
 entry "Chainload (hd0)" {
@@ -22,4 +26,4 @@ EOF
 
 mkisofs -J -R -l -b boot/cdboot.img -V "CDROM" -boot-load-size 4 -boot-info-table -no-emul-boot -o build/x86-pc/test.iso isobuild
 rm -rf isobuild
-qemu -cdrom build/x86-pc/test.iso -serial stdio -vga std -boot d -m 512
+qemu-system-x86_64 -cdrom build/x86-pc/test.iso -serial stdio -vga std -boot d -m 512
