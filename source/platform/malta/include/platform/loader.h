d2b547e55e0afbe5cae3a959e1a10aabad2d2238
Alex Smith <alex@alex-smith.me.uk>
2013-10-13 11:23:58 +0100

    Malta: Move load address to avoid conflicting with YAMON
    On real hardware (or if using YAMON in QEMU) the old load address was
    in the middle of where YAMON would be, so load somewhere else instead.
    
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

--- source/platform/malta/include/platform/loader.h
+++ source/platform/malta/include/platform/loader.h
@@ -22,8 +22,17 @@
 #ifndef __PLATFORM_LOADER_H
 #define __PLATFORM_LOADER_H
 
-/** Load address (start of RAM, KSEG0). */
-#define LOADER_LOAD_ADDR		0x80010000
+/**
+ * Load address.
+ *
+ * As far as I can tell the low 1MB is pretty much unusable. Most of it is
+ * either taken by YAMON or hardware. We need to try and stay out of the way
+ * of where a kernel might want to load to, so go for a slightly obscure
+ * location: 31MB into physical memory (mapped in KSEG0). Unless someone tries
+ * to load a really big kernel image, or load their kernel at a silly location,
+ * it seems unlikely that this will conflict.
+ */
+#define LOADER_LOAD_ADDR		0x81f00000
 
 #ifndef __ASM__
 
